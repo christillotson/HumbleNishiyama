59f2883259e01e32a56d9a281d34a9376daff964
Christopher Tillotson <167741985+christillotson@users.noreply.github.com>
2025-09-17 18:34:30 -0400

    Pushing modified code for method 2
    Includes data changed and slimmed down by deleting non-branch related files.

--- run_tests.py
+++ run_tests.py
@@ -1,11 +1,11 @@
 # TESTING OUT METHOD ONE and its ability to generate the data into the data/method_1 folder
 
-from src.method_1 import DecksInt
+from src.method_2 import DecksStr
 
 NUM_DECKS_TO_TEST = [1_000, 10_000, 100_000, 1_000_000, 2_000_000]
 
 for num_decks in NUM_DECKS_TO_TEST:
-    decks = DecksInt(num_decks=num_decks)
+    decks = DecksStr(num_decks=num_decks)
     print(decks.decks.shape)       # (10, 52)
     #print(decks_2million.decks.sum(axis=1)) # each row sums to 26
     print(decks.decks.nbytes, "bytes")

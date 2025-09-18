5475f71df206edd3f2b7c2a8921cb0761820b4b7
Christopher Tillotson <167741985+christillotson@users.noreply.github.com>
2025-09-18 18:59:02 -0400

    Adding method 2 to main
    Can't merge branch because initially did it wrong by deleting already existing method_1.py, so that would delete it - can instead just add these files and should function normally.

--- run_tests.py
+++ run_tests.py
@@ -13,3 +13,19 @@ for num_decks in NUM_DECKS_TO_TEST:
     print('---')
 
 # Method 1 works and generates the files, even if data directory is missing, so it is safe to delete it.
+
+# TESTING OUT METHOD TWO and its ability to generate the data into the data/method_2 folder
+
+from src.method_2 import DecksStr
+
+NUM_DECKS_TO_TEST = [1_000, 10_000, 100_000, 1_000_000, 2_000_000]
+
+for num_decks in NUM_DECKS_TO_TEST:
+    decks = DecksStr(num_decks=num_decks)
+    print(decks.decks.shape)       # (10, 52)
+    #print(decks_2million.decks.sum(axis=1)) # each row sums to 26
+    print(decks.decks.nbytes, "bytes")
+    print(decks.decks.nbytes / (1024**2), "MB")
+    print('---')
+
+# This will create the data directory for method_2 if it does not exist, so it is safe to delete the data folder.
---
+++ src/method_2.py
@@ -0,0 +1,67 @@
+import sys
+import os
+import numpy as np
+import random
+
+class DecksStr:
+  """
+  class that generates a numpy array of numpy arrays of 'decks of cards.'
+  The decks of cards are actually represented by the integers 0 and 1 combined into one long integer
+  and these 'decks of cards' are saved in .npz files with a specified maximum of 500,000 decks per file.
+  """
+  def __init__(self, num_decks, random_seed = 440, write_decks_to_file = True, base_path = "./data/method_2/"):
+    self.num_decks = num_decks
+    self.random_seed = random_seed
+    self.rng = np.random.default_rng(random_seed)
+    self.decks = self.make_many_decks(self.num_decks)
+    self.base_path = base_path
+
+    if write_decks_to_file:
+            self.save_decks()
+
+  def make_one_deck(self) -> np.array:
+    """
+    Function that makes a numpy array of 26 0s and 1s (in integer format), 
+    then shuffles it based on the prior defined random seed.
+    """
+    deck = np.repeat(['0', '1'], 26)
+    self.rng.shuffle(deck)
+
+    ## special stuff that will take a long time maybe for DecksStr but hopefully compress better?
+    begin_one_list = ['1'] + deck
+    joined_deck = int(''.join(begin_one_list))
+    return joined_deck
+
+  def make_many_decks(self, num_decks: int) -> np.array:
+    """
+    Function that makes a numpy array *of* numpy arrays, each containing a randomized deck of 'cards' (0 and 1 integers).
+    num_decks (int): the number of decks to be generated.
+    """
+    deck_list = np.array([self.make_one_deck() for i in range(num_decks)])
+    return deck_list
+
+  def save_decks(self, max_decks_per_file = 500_000) -> None:
+      """
+      Function that, if specified, will save the generated decks as .npy files in the data/method_2 directory into subfolders identifying 
+      1. the number of decks generated
+      2. the random seed of those generated decks.
+      The function will generate the subfolder if it doesn't exist, 
+      and tests run with the same num_decks and random seed will overwrite existing files.
+      max_decks_per_file specifies how many decks of cards will exist in one file, for example 
+      2,000,000 decks will be divided into 4 files. This is in order to respect GitHub file size limits.
+      """
+      num_decks = self.decks.shape[0]
+      
+      os.makedirs(self.base_path, exist_ok=True)
+      subdir = os.path.join(self.base_path, f"{num_decks}_decks_seed_{self.random_seed}")
+      os.makedirs(subdir, exist_ok=True) 
+            
+      for i in range(0, num_decks, max_decks_per_file):
+          # chunk = self.decks[i : i + max_decks_per_file]
+          # filename = f"{base_filepath}decks_{i}-{i + chunk.shape[0]}.npz"
+          chunk = self.decks[i : i + max_decks_per_file]
+          start, end = i, i + chunk.shape[0] - 1
+          filename = os.path.join(subdir, f"decks_{start+1}-{end+1}.npz")
+          np.savez_compressed(filename, decks=chunk)
+          print(f"Saved {filename} with {chunk.shape[0]} decks")
+      return None

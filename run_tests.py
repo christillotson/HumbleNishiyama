990edc63db1b32b0826878eda9ffe6a7330250e3
kdlen11 <157778643+kdlen11@users.noreply.github.com>
2025-09-18 20:38:05 -0400

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,31 +1,26 @@
-# TESTING OUT METHOD ONE and its ability to generate the data into the data/method_1 folder
+import numpy as np
+from src.run_methods_as_functions import method_1, method_2
 
-from src.method_1 import DecksInt
-
+# we will run 5 experiments, on 5 different numbers of decks of cards
 NUM_DECKS_TO_TEST = [1_000, 10_000, 100_000, 1_000_000, 2_000_000]
 
-for num_decks in NUM_DECKS_TO_TEST:
-    decks = DecksInt(num_decks=num_decks)
-    print(decks.decks.shape)       # (10, 52)
-    #print(decks_2million.decks.sum(axis=1)) # each row sums to 26
-    print(decks.decks.nbytes, "bytes")
-    print(decks.decks.nbytes / (1024**2), "MB")
-    print('---')
+# and for each number of cards we will get the mean and stdev for a certain number of repetitions.
+# there will be more repititions for smaller values of num_decks, and fewer repitions for larger values of num_decks.
+NUM_TIMES_TO_RUN_EXPERIMENT = [30, 30, 20, 10, 10]
 
-# Method 1 works and generates the files, even if data directory is missing, so it is safe to delete it.
-
-# TESTING OUT METHOD TWO and its ability to generate the data into the data/method_2 folder
-
-from src.method_2 import DecksStr
-
-NUM_DECKS_TO_TEST = [1_000, 10_000, 100_000, 1_000_000, 2_000_000]
+# we are going to start at random seed 440, and add 1 to that seed with each iteration.
+random_seed = 440
 
 for num_decks in NUM_DECKS_TO_TEST:
-    decks = DecksStr(num_decks=num_decks)
-    print(decks.decks.shape)       # (10, 52)
-    #print(decks_2million.decks.sum(axis=1)) # each row sums to 26
-    print(decks.decks.nbytes, "bytes")
-    print(decks.decks.nbytes / (1024**2), "MB")
-    print('---')
+    for rep in NUM_TIMES_TO_RUN_EXPERIMENT:
+        random_seed += 1
+        decks_method_1 = method_1(num_decks=num_decks, random_seed=random_seed)
+        decks_method_2 = method_2(num_decks=num_decks, random_seed=random_seed)
 
-# This will create the data directory for method_2 if it does not exist, so it is safe to delete the data folder.
+# for num_decks in NUM_DECKS_TO_TEST:
+#     decks = DecksStr(num_decks=num_decks)
+#     print(decks.decks.shape)       # (10, 52)
+#     #print(decks_2million.decks.sum(axis=1)) # each row sums to 26
+#     print(decks.decks.nbytes, "bytes")
+#     print(decks.decks.nbytes / (1024**2), "MB")
+#     print('---')

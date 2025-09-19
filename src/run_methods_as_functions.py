bcc7ae3af4a9b7201f911fd5a50e192a56c97470
kdlen11 <157778643+kdlen11@users.noreply.github.com>
2025-09-18 20:32:04 -0400

    Create run_methods_as_functions.py

---
+++ src/run_methods_as_functions.py
@@ -0,0 +1,23 @@
+# code that runs methods 1 and 2 for the metrics:
+# run + write times, storage space, memory usage, and read times
+
+from src.method_1 import DecksInt
+from src.method_2 import DecksStr
+
+def method_1(num_decks, random_seed, write_decks_to_file = True, base_path = "./data/method_1/"):
+    """
+    a function that runs the class DecksInt (method_1).
+    The reason for this is so that the wrapper (which works on functions) that runs quantitative tests on our output
+    can actually run effectively. 
+    """
+    decks = DecksInt(num_decks, random_seed, write_decks_to_file = True, base_path = "./data/method_1/")
+    return decks
+
+def method_2(num_decks, random_seed, write_decks_to_file = True, base_path = "./data/method_2/"):
+    """    
+    a function that runs the class DecksStr (method_2).
+    The reason for this is so that the wrapper (which works on functions) that runs quantitative tests on our output
+    can actually run effectively.    
+    """
+    decks = DecksStr(num_decks, random_seed, write_decks_to_file = True, base_path = "./data/method_2/")
+    return decks

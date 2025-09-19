1d07ce77ab5700f8f8f47bd9f7e712a72d40504f
kdlen11 <157778643+kdlen11@users.noreply.github.com>
2025-09-18 22:16:26 -0400

    deleting the summary log file if it exists in the directory when another test is run

--- run_tests.py
+++ run_tests.py
@@ -3,12 +3,16 @@ import numpy as np
 from src.run_methods_as_functions import method_1_funct, method_2_funct
 from src.results import summarize_experiments_to_file
 
-# deleting wrapper_log.txt if it already exists, so that any tests run in the program are the only ones recorded.
+# deleting wrapper_log.txt and experiment_summary.txt files if they already exist, so that any tests run in the program are the only ones recorded.
 
 wrapper_log_path = "./data/wrapper_log.txt"
+summary_log_path = "./data/experiment_summary.txt"
 if os.path.exists(wrapper_log_path):
     os.remove(wrapper_log_path)
     print("We found an existing test log file with data already in it -- it has been deleted.")
+if os.path.exists(summary_log_path):
+    os.remove(summary_log_path)
+    print("We found an existing statistical summary log file with data already in it -- it has been deleted.")
 
 # we will run 5 experiments, on 5 different numbers of decks of cards
 NUM_DECKS_TO_TEST = [1_000, 10_000, 100_000]
@@ -21,10 +25,8 @@ NUM_TIMES_TO_RUN_EXPERIMENT = [30, 15, 10]
 random_seed = 440
 
 for i, num_decks in enumerate(NUM_DECKS_TO_TEST):
-    print(f'Generating {num_decks} decks {NUM_TIMES_TO_RUN_EXPERIMENT[i]} times...')
+    print(f'Generating {num_decks} decks {NUM_TIMES_TO_RUN_EXPERIMENT[i]*2} times ({NUM_TIMES_TO_RUN_EXPERIMENT[i]} times for each of 2 methods)...')
     for rep in range(NUM_TIMES_TO_RUN_EXPERIMENT[i]):
         random_seed += 1
         decks_method_1 = method_1_funct(num_decks=num_decks, random_seed=random_seed)
         decks_method_2 = method_2_funct(num_decks=num_decks, random_seed=random_seed) 
-
-summarize_experiments_to_file(wrapper_log_path)
